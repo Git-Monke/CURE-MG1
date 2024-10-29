ca2bac4190cb2a3e310261a6feb22bba8ba0a2cc
Git-Monke <javelasq2006@gmail.com>
2024-10-29 08:23:47 -0700

    Changed icons from grid to randomly positioned

--- icons/src/routes/+page.svelte
+++ icons/src/routes/+page.svelte
@@ -1,1 +1,58 @@
-<h1 class="bg-red-500 text-white p-4">Hello Tailwind</h1>
+<script>
+  import { writable } from "svelte/store";
+  import icon_names from "./icon_names";
+  import { onMount } from "svelte";
+
+  let size;
+  let object;
+  let icons;
+
+  class Icon {
+    constructor(x, y, name) {
+      Object.assign(this, { x, y, name });
+    }
+  }
+
+  function randIcons(n) {
+    let icon_list = [];
+    for (let i = 0; i < n; i++) {
+      let rand_icon_name =
+        icon_names[Math.floor(Math.random() * icon_names.length)];
+      let x = Math.floor(Math.random() * (size.width - 64));
+      let y = Math.floor(Math.random() * (size.height - 64));
+
+      icon_list.push(new Icon(x, y, rand_icon_name));
+    }
+
+    icons = icon_list;
+  }
+
+  onMount(() => {
+    let { width, height } = object.getBoundingClientRect();
+    size = { width, height };
+
+    randIcons(5);
+  });
+</script>
+
+<div class="flex flex-col h-screen">
+  <h1 class="border-b-4 h-[5vh] flex items-center justify-center">
+    Memory Game
+  </h1>
+
+  <div class="w-full h-full p-10">
+    <div
+      bind:this={object}
+      class="bg-white w-full h-full border-4 rounded-lg relative"
+    >
+      {#each icons as icon}
+        <img
+          src={`/icons/${icon.name}.svg`}
+          style={`position: absolute; top: ${icon.y}px; left: ${icon.x}px;`}
+          class="w-16 h-16"
+          alt="Game requires images"
+        />
+      {/each}
+    </div>
+  </div>
+</div>
---
+++ icons/src/routes/game.svelte
@@ -0,0 +1,18 @@
+<!-- <script>
+    import { onMount } from "svelte";
+    import {writeable} from "svelte/store"
+
+  export let icon_data = [];
+  export const size = writeable({width: 0, height: 0})
+
+  let container;
+
+  onMount(() => {
+    const { width, height } = container.getBoundingClientRect()
+    size.set({width, height})
+  })
+</script>
+
+{#each icon_data as icon}
+    <img src=>
+{/each} -->
